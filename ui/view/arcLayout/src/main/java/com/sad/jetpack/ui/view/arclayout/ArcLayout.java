fd49d08f5f7c44a46b0e6fb5c316d44732c87fa1
lucifinilsu <lucifinilsu@gmail.com>
2020-05-08 18:19:55 +0800

    新增弧顶布局项目,新增属性

--- ui/view/arcLayout/src/main/java/com/sad/jetpack/ui/view/arclayout/ArcLayout.java
+++ ui/view/arcLayout/src/main/java/com/sad/jetpack/ui/view/arclayout/ArcLayout.java
@@ -4,13 +4,16 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.PaintFlagsDrawFilter;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 
@@ -22,8 +25,10 @@ public class ArcLayout extends RelativeLayout {
     private int arcHeight;
     private int mBgColor;
 
-    private Path clipPath;
     private Paint paint;
+    public static final int TOP=1;
+    public static final int BOTTOM=2;
+    private int posType=TOP;
     public ArcLayout(Context context) {
         this(context, null);
     }
@@ -37,58 +42,71 @@ public class ArcLayout extends RelativeLayout {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ArcLayout);
         arcHeight = typedArray.getDimensionPixelSize(R.styleable.ArcLayout_arcHeight, 0);//dipToPx(getContext(),typedArray.getDimensionPixelSize(R.styleable.ArcLayout_arcHeight, 0));
         mBgColor=typedArray.getColor(R.styleable.ArcLayout_arcColor, Color.parseColor("#ffffffff"));
-        
+        posType = typedArray.getInt(R.styleable.ArcLayout_arcPosType, TOP);
+        if (paint==null){
+            paint = new Paint();
+        }
+
         typedArray.recycle();
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
             setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         }
-        if (paint==null){
-            paint = new Paint();
-        }
-        paint.setStyle(Paint.Style.FILL);
-        paint.setAntiAlias(true);
+        setWillNotDraw(false);
+
     }
     public void setArcHeight(int arcHeight){
         this.arcHeight=arcHeight;
-        invalidate();
+        requestLayout();
     }
-    private Path createClipPath() {
-        final Path path = new Path();
 
-        /*path.moveTo(0, 0);
-        path.lineTo(0, height);
-        path.quadTo(width / 2, height - 2 * arcHeight, width, height);
-        path.lineTo(width, 0);
-        path.close();*/
-
-        path.moveTo(0,arcHeight);
-        path.quadTo(width / 2,-arcHeight,width,arcHeight);
-        path.close();
-
-
-        return path;
+    public void setPosType(int posType) {
+        this.posType = posType;
+        requestLayout();
     }
+
+    public int getArcHeight() {
+        return arcHeight;
+    }
+
+    public int getPosType() {
+        return posType;
+    }
+
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
         if (changed) {
-            calculateLayout();
-            //LogUtils.e("--------->mHeight="+height+",mArcHeight="+arcHeight);
+            height = getMeasuredHeight();
+            width = getMeasuredWidth();
+            if (height<arcHeight){
+                arcHeight=height;
+            }
+            //Log.e("arc","--------->mHeight="+height+",mArcHeight="+arcHeight+",w="+width);
         }
     }
-    private void calculateLayout() {
-        height = getMeasuredHeight();
-        width = getMeasuredWidth();
-        if (width > 0 && height > 0) {
-            clipPath = createClipPath();
-        }
-    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        Path path = new Path();
+        if (this.posType==TOP){
+            path.moveTo(0,arcHeight);
+            path.quadTo(width / 2,-arcHeight,width,arcHeight);
+        }
+        else if (posType==BOTTOM){
+            path.moveTo(0,height);
+            path.quadTo(width / 2,height-2*arcHeight,width,height);
+        }
+        path.close();
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setAntiAlias(true);
         paint.setColor(mBgColor);
-        //paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+
         canvas.setDrawFilter(new PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG));
-        canvas.drawPath(clipPath, paint);
+        canvas.drawPath(path, paint);
     }
+
 }
